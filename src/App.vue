b18a7c121636e6d769f4db3b627b1002fc750c1f
Lucien <1964450730@qq.com>
2021-09-25 14:44:28 +0800

    111

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
     <router-view/>
+    <main-tab-bar></main-tab-bar>
   </div>
 </template>
 
 <script>
+import MainTabBar from "@/components/content/mainTabbar/MainTabBar.vue";
 export default {
- name:'app'
- 
+ name:'app',
+ components: {
+      MainTabBar
+    }
 }
 </script>
 
 <style>
-
+  @import "./assets/css/base.css";
 </style>
