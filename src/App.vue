4a8db1a18b4acdb0de39403c284803439e5da388
Lucien <1964450730@qq.com>
2021-09-27 22:53:52 +0800

    ..

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,36 @@
 <template>
   <div id="app">
-    <router-view />
-    <main-tab-bar />
+    <img src="./assets/logo.png">
+    <div>
+      <p>
+        If ViewUI is successfully added to this project, you'll see an
+        <code v-text="'<Button>'"></code>
+        below
+      </p>
+      <Button type="primary">Button</Button>
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import MainTabBar from "@/components/content/mainTabbar/MainTabBar.vue";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: "app",
+  name: 'app',
   components: {
-    MainTabBar,
-  },
-};
+    HelloWorld
+  }
+}
 </script>
 
 <style>
-  @import "./assets/css/base.css";
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import {request} from "./network/request"
+import './plugins/iview.js'
 
 Vue.config.productionTip = false
 
