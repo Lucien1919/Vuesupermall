abe52520ad0010baf9347aa9334774b8a22806b3
Lucien <1964450730@qq.com>
2021-10-10 20:23:40 +0800

    ...

---
+++ src/components/common/tabControl/TabControl.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="TabControl">
+    <div class="TabControlItem" v-for="(item,index) in titles" @click="tabActive(index)">
+      <span :class="{active:ActiveNum == index}">{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabControl",
+
+  data() {
+    return {
+      ActiveNum:0
+    }
+  },
+  props: {
+    titles: [],
+  },
+
+  methods: {
+    tabActive(index) {
+      this.ActiveNum = index;
+    },
+  },
+};
+</script>
+
+<style scope>
+.TabControl {
+  display: flex;
+}
+
+.TabControlItem {
+  flex: 1;
+  text-align: center;
+}
+
+.active {
+  border-bottom: var(--color-tint) solid 3px;
+  padding: 3px;
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -7,6 +7,7 @@
     <swiper :banners="banners"></swiper>
     <recom-view :recommends="recommends"></recom-view>
     <feature-view></feature-view>
+    <tab-control class="tabControl" :titles="titles"></tab-control>
   </div>
 </template>
 
@@ -16,24 +17,29 @@ import NavBar from "components/common/navBar/NavBar.vue";
 import Swiper from "components/common/swiper/Swiper.vue";
 import RecomView from "components/common/recomView/RecomView.vue";
 import FeatureView from "./FeatureView.vue";
+import TabControl from 'components/common/tabControl/TabControl.vue';
 
 export default {
-  components: { NavBar, Swiper, RecomView, FeatureView },
+  components: { NavBar, Swiper, RecomView, FeatureView, TabControl },
   name: "Home",
 
   data() {
     return {
       banners: [],
       recommends: [],
+      titles:["流行","新款","精选"]
     };
   },
   created() {
+
     //异步操作，只能在里面获取数据
     getHomeMultidata().then((res) => {
       console.log(res);
       this.banners = res.data.banner.list;
       this.recommends = res.data.recommend.list;
     });
+
+
   },
 };
 </script>
@@ -45,12 +51,17 @@ export default {
 }
 .home{
   padding-top: 44px;
-  padding-bottom: 500px;
+  padding-bottom: 1000px;
 }
 .homeBar{
   position:fixed;
   top: 0;
   left: 0;
   z-index: 9;
+}
+
+.tabControl{
+  position: sticky;
+  top: 44px;
 }
 </style>
